745d641d232f2d285873dd20b38efe9b52051a02
Sterling Walden <155565272+sterlingwalden@users.noreply.github.com>
2024-11-17 04:22:56 +0000

    Python Basic Syntax

---
+++ 1 Python Basic Syntax/1.1 Print Statements/print_statement.py
@@ -0,0 +1,10 @@
+# Print statements are used to display information to the console
+print("Hello, World!")
+
+# You can use the print function to print multiple functions
+print("The sum of", 1, "and", 1, "equals", 1 + 1)
+
+# You can add an 'f' string instead of adding commas to simplify your print-out. Use {} to encapsulate the function you want to print.
+name = "Bob"
+age = 27
+print(f"My name is {name}, and I am {age} years old.")
---
+++ 1 Python Basic Syntax/1.2 Comments/comments.py
@@ -0,0 +1,24 @@
+# Comments are used to describe what the code does and is not interpreted by the interpreter.
+
+# Single Line Comment
+# This is a comment on the code (It's good)
+print("Good Print")
+
+# Multi-Line Comments
+# Python uses """ or ''' to denote multi-line comments like this
+
+"""
+
+This is  a multi-line comment
+It goes for multiple lines
+
+"""
+
+'''
+
+This is also a multi-line comment.
+It also spans for multiple lines!
+
+'''
+
+
